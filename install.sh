a4da094a944fff487a802e885a3590165bc553a4
macexx <marcus.freiholtz@gmail.com>
2021-03-26 21:40:43 +0100

    Upgrade to v3.2.14

--- install.sh
+++ install.sh
@@ -99,10 +99,10 @@ chmod -R +x /etc/my_init.d/
 #########################################
 
 cd /tmp
-wget https://static.tp-link.com/2020/202004/20200420/Omada_Controller_v3.2.10_linux_x64.tar.gz
-tar zxvf Omada_Controller_v3.2.10_linux_x64.tar.gz
-chown -R root:root /tmp/Omada_Controller_v3.2.10_linux_x64
-cd Omada_Controller_v3.2.10_linux_x64
+wget https://static.tp-link.com/2020/202012/20201225/Omada_Controller_v3.2.14_linux_x64.tar.gz
+tar zxvf Omada_Controller_v3.2.14_linux_x64.tar.gz
+chown -R root:root /tmp/Omada_Controller_v3.2.14_linux_x64
+cd Omada_Controller_v3.2.14_linux_x64/
 chmod +x install.sh
 sed -i '203d' install.sh
 echo yes | ./install.sh
